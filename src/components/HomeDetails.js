606051e473c7172f539602fdfbc0e3c24971103e
jftbc <jf@tenbruggencate.net>
2018-11-06 14:44:48 +0100

    add Homedetails files

---
+++ src/components/HomeDetails.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import logo from '../logo.svg'
+
+
+export default function HomeDetails(props) {
+    const home=props.home
+    return (
+        <div>
+        <img src = {logo} alt="portrait" />
+
+            <h2>{home.city}</h2>
+            <h3>{home.price}</h3>
+            <p>{home.description}</p>
+        </div>
+    )
+}
